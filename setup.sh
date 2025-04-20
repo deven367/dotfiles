cc87e4e8ae49edebbef6b6faab04789171987197
deven367 <masterdeven@gmail.com>
2025-04-20 11:57:16 -0400

    mv `oh-my-zsh` setup to the bottom

--- setup.sh
+++ setup.sh
@@ -47,29 +47,6 @@ if [ $ans = "y" ]; then
 fi
 
 
-# if shell is zsh, then install oh-my-zsh
-if [ $SHELL = "/bin/zsh" ]; then
-    echo -n "install/configure oh-my-zsh? [y/n]: "
-    read ans
-
-    if [ $ans = "y" ]; then
-        # install oh-my-zsh
-        echo "installing oh-my-zsh"
-        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-        echo "done"
-
-
-        aliases="if [ -f ~/.bash_aliases ]; then
-            . ~/.bash_aliases
-        fi"
-
-        # write var to file
-        echo "$aliases" >> ~/.zshrc
-
-
-    fi
-fi
-
 # create a symbolic link to the .bash_aliases file
 echo -n "create symbolic link to .bash_aliases? [y/n]: "
 read ans
@@ -100,4 +77,27 @@ if [ $ans = "y" ]; then
     wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O ~/bin/yt-dlp
     chmod a+rx ~/bin/yt-dlp  # Make executable
     echo "done"
+fi
+
+# if shell is zsh, then install oh-my-zsh
+if [ $SHELL = "/bin/zsh" ]; then
+    echo -n "install/configure oh-my-zsh? [y/n]: "
+    read ans
+
+    if [ $ans = "y" ]; then
+        # install oh-my-zsh
+        echo "installing oh-my-zsh"
+        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        echo "done"
+
+
+        aliases="if [ -f ~/.bash_aliases ]; then
+            . ~/.bash_aliases
+        fi"
+
+        # write var to file
+        echo "$aliases" >> ~/.zshrc
+
+
+    fi
 fi
